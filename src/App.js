11131d501344e663935eebd1581f0690f875655f
richforest0506 <richforest0506@gmail.com>
2022-04-01 22:48:08 -0400

    Additional styling & added the suscribe box

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <main>
       <Header />
-      {/* <Suscribe /> */}
+      <Suscribe />
     </main>
   )
 }
--- src/components/Suscribe.js
+++ src/components/Suscribe.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import '../styles/Suscribe.css'
+import Border from '../assets/images/boxCorner.png'
 
 const Suscribe = () => {
-  return <div>suscribe</div>
+  return (
+    <div className="suscribe">
+      <img src={Border} alt="" />
+      <p>
+        <span>Suscribe</span>
+        to our newsletter:
+      </p>
+      <input type="text" placeholder="Enter your name..." />
+    </div>
+  )
 }
 
 export default Suscribe
