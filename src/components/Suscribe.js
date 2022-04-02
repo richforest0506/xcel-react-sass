26424f98db61db4d01e3f8c23292cba69082273b
richforest0506 <richforest0506@gmail.com>
2022-04-02 13:17:27 -0400

    Added useState/useEffect into suscribe form, needs work

--- src/components/Suscribe.js
+++ src/components/Suscribe.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import '../styles/Suscribe.css'
 import Border from '../assets/images/boxCorner.png'
 
 const Suscribe = () => {
+  const [isActive, setActive] = useState(false)
+
+  useEffect(() => {
+    !isActive
+      ? (document.querySelector('button').style.visibility = 'hidden')
+      : (document.querySelector('button').style.visibility = 'visible')
+  }, [isActive])
+
+  function thing() {
+    !isActive ? setActive(true) : setActive(false)
+    console.log(isActive)
+  }
+
   return (
     <div className="suscribe">
       <img src={Border} alt="" />
@@ -10,7 +23,15 @@ const Suscribe = () => {
         <span>Suscribe</span>
         to our newsletter:
       </p>
-      <input type="text" placeholder="Enter your name..." />
+      <form>
+        <input
+          type="text"
+          placeholder="Enter your name..."
+          onMouseEnter={() => thing()}
+          onMouseLeave={() => thing()}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   )
 }
